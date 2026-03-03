54b501831d92da24113d9fbdad0b78fbfc069778
vishal0200 <vishalbelwal1234@gmail.com>
2026-03-03 16:37:38 +0530

    Solved Sort Colors using two pointer approach

---
+++ two_pointers/Sort Colors.cpp
@@ -0,0 +1,62 @@
+/*Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int left = 0;
+       int right = nums.size() - 1;
+       int mid = 0;
+
+       while(mid <= right){
+        if(nums[mid] == 0){
+            swap(nums[mid],nums[left]);
+            left++;
+            mid++;
+        }
+
+        else if(nums[mid] == 1){
+            mid++;
+        }
+
+        else{ //nums[mid] == 2
+            swap(nums[mid],nums[right]);
+            right--;
+        }
+       }
+    }
+};
+
+int main(){
+    vector<int> nums{2, 0, 2, 1, 1, 0};
+
+    Solution sol;
+
+    sol.sortColors(nums);
+
+    cout << "Modifies Array : ";
+    for(int i : nums){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
